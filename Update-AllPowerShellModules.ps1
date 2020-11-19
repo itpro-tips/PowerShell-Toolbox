92951a856605f6ec6a83d8ae4e04a15d27942471
itpro-tips <44789311+itpro-tips@users.noreply.github.com>
2020-11-19 17:48:06 +0100

    Update Update-AllPowerShellModules.ps1

--- Update-AllPowerShellModules.ps1
+++ Update-AllPowerShellModules.ps1
@@ -1,3 +1,9 @@
+# https://itpro-tips.com/2020/update-all-powershell-modules-at-once/
+# https://itpro-tips.com/2020/mettre-a-jour-tous-les-modules-powershell-en-une-fois/
+
+# This script provide informations about the module version (current and the latest available on PowerShell Gallery) and update to the latest version
+# If you have a module with two or more versions, the script delete them and reinstall only the latest.
+
 # PowerShell 5.0 pour PowerShell Gallery  
 #Requires -Version 5.0
 #Requires -RunAsAdministrator
@@ -22,7 +28,7 @@ foreach ($module in $modules.Name) {
     $moduleInfos = Find-Module -Name $module
 	
     if ($null -eq $currentVersion) {
-        Write-Host -ForegroundColor Cyan "Install from PowerShellGallery : $($moduleInfos.Name) - $($moduleInfos.Version). Release date:$($moduleInfos.PublishedDate)"  
+        Write-Host -ForegroundColor Cyan "Install from PowerShellGallery : $($moduleInfos.Name) - $($moduleInfos.Version). Release date: $($moduleInfos.PublishedDate)"  
 		
         try {
             Install-Module -Name $module -Force
@@ -35,17 +41,17 @@ foreach ($module in $modules.Name) {
         Write-Host -ForegroundColor Green "$($moduleInfos.Name) already installed in the latest version ($currentVersion. Release date: $($moduleInfos.PublishedDate))"
     }
     elseif ($currentVersion.count -gt 1) {
-        Write-Warning "$module is installed in $($currentVersion.count) versions (versions: $currentVersion)"
-        Write-Host -ForegroundColor Cyan "Uninstall all $module PowerShell module versions"
+        Write-Warning "$module is installed in $($currentVersion.count) versions (versions: $($currentVersion -join ' | '))"
+        Write-Host -ForegroundColor Cyan "Uninstall previous $module versions"
         
         try {
-            Get-InstalledModule -Name $module -AllVersions | Uninstall-Module -Force
+            Get-InstalledModule -Name $module -AllVersions | Where-Object {$_.Version -ne $moduleInfos.Version} | Uninstall-Module -Force
         }
         catch {
             Write-Host -ForegroundColor red "$_.Exception.Message"
         }
         
-        Write-Host -ForegroundColor Cyan "Install from PowerShellGallery : $($moduleInfos.Name) - $($moduleInfos.Version). Release date:$($moduleInfos.PublishedDate)"  
+        Write-Host -ForegroundColor Cyan "Install from PowerShellGallery : $($moduleInfos.Name) - $($moduleInfos.Version). Release date: $($moduleInfos.PublishedDate)"  
     
         try {
             Install-Module -Name $module -Force
@@ -55,7 +61,7 @@ foreach ($module in $modules.Name) {
         }
     }
     else {       
-        Write-Host -ForegroundColor Cyan "Update from PowerShellGallery from $currentVersion to $($moduleInfos.Name) - $($moduleInfos.Version). Release date:$($moduleInfos.PublishedDate)" 
+        Write-Host -ForegroundColor Cyan "Update from PowerShellGallery from $currentVersion to $($moduleInfos.Name) - $($moduleInfos.Version). Release date: $($moduleInfos.PublishedDate)" 
         try {
             Update-Module -Name $module -Force
         }
