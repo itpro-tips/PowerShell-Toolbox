e8aff1e83620520a18675a380f43df74228a5be2
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2022-09-30 10:55:45 +0200

    Add New-FlatObject

---
+++ New-FlatObject.ps1
@@ -0,0 +1,31 @@
+﻿function New-FlatObject {
+    Param 
+    (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        $object
+    )
+
+    process {
+        $returnHashTable = [ordered]@{ }
+        foreach ($prop in $object.psobject.Properties) {
+            if ($prop.Value -is [array]) {
+                #if (($prop.Value -ne $null) -and (-not $prop.Value.GetType().isValueType))
+                $counter = 0
+                foreach ($value in $prop.Value) {
+                    if ($value -is [array]) {
+                        #if (($prop.Value -ne $null) -and (-not $prop.Value.GetType().isValueType)) 
+                        foreach ($recurse in (New-FlatObject -object $value).psobject.Properties) {
+                            $returnHashTable["$($prop.Name)-$($recurse.Name)"] = $recurse.Value
+                        }
+                    }
+                    $returnHashTable["$($prop.Name)-$counter"] = $value
+                    $counter++
+                }
+            }
+            else {
+                $returnHashTable[$prop.Name] = $prop.Value
+            }
+        }
+        return [PSCustomObject]$returnHashTable | Sort-Object @{Expression = { (($_.psobject.properties) | Measure-Object).count } } -Descending
+    }
+}
