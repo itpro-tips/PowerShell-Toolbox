bbe241423e64f9c370e411703cb27370fd510622
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2023-01-20 11:52:02 +0100

    Update Get-MSIFileInformation.ps1

--- Get-MSIFileInformation.ps1
+++ Get-MSIFileInformation.ps1
@@ -1,48 +1,100 @@
 ﻿function Get-MSIFileInformation {
     param(
-        [parameter(Mandatory = $true)]
-
-        [ValidateNotNullOrEmpty()] [System.IO.FileInfo]$Path
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.IO.FileInfo[]]$FilePath
     ) 
-    Process {
-        $properties = @('ProductVersion', 'ProductCode', 'ProductName', 'Manufacturer', 'ProductLanguage', 'FullVersion')
+  
+    $productLanguageHashTable = @{
+        '1025' = 'Arabic'
+        '1026' = 'Bulgarian'
+        '1027' = 'Catalan'
+        '1028' = 'Chinese - Traditional'
+        '1029' = 'Czech'
+        '1030' = 'Danish'
+        '1031' = 'German'
+        '1032' = 'Greek'
+        '1033' = 'English'
+        '1034' = 'Spanish'
+        '1035' = 'Finnish'
+        '1036' = 'French'
+        '1037' = 'Hebrew'
+        '1038' = 'Hungarian'
+        '1040' = 'Italian'
+        '1041' = 'Japanese'
+        '1042' = 'Korean'
+        '1043' = 'Dutch'
+        '1044' = 'Norwegian'
+        '1045' = 'Polish'
+        '1046' = 'Brazilian'
+        '1048' = 'Romanian'
+        '1049' = 'Russian'
+        '1050' = 'Croatian'
+        '1051' = 'Slovak'
+        '1053' = 'Swedish'
+        '1054' = 'Thai'
+        '1055' = 'Turkish'
+        '1058' = 'Ukrainian'
+        '1060' = 'Slovenian'
+        '1061' = 'Estonian'
+        '1062' = 'Latvian'
+        '1063' = 'Lithuanian'
+        '1081' = 'Hindi'
+        '1087' = 'Kazakh'
+        '2052' = 'Chinese - Simplified'
+        '2070' = 'Portuguese'
+        '2074' = 'Serbian'
+    }
+
+    $properties = @('ProductVersion', 'ProductCode', 'ProductName', 'Manufacturer', 'ProductLanguage', 'FullVersion')
    
-        $object = [PSCustomObject][ordered]@{
-            File = $Path.Name
+    try {
+        $file = Get-ChildItem $FilePath -ErrorAction Stop
+    }
+    catch {
+        Write-Warning "Unable to get file $FilePath $($_.Exception.Message)"
+        return
+    }
+
+    $object = [PSCustomObject][ordered]@{
+        FileName     = $file.Name
+        FilePath     = $file.FullName
+        'Length(MB)' = $file.Length / 1MB
+    }
+
+    # Read property from MSI database
+    $windowsInstallerObject = New-Object -ComObject WindowsInstaller.Installer
+    $MSIDatabase = $windowsInstallerObject.GetType().InvokeMember('OpenDatabase', 'InvokeMethod', $null, $windowsInstallerObject, @($file.FullName, 0))
+
+    foreach ($property in $properties) {
+        $view = $null
+        $query = "SELECT Value FROM Property WHERE Property = '$($property)'"
+        $view = $MSIDatabase.GetType().InvokeMember('OpenView', 'InvokeMethod', $null, $MSIDatabase, ($query))
+        $view.GetType().InvokeMember('Execute', 'InvokeMethod', $null, $view, $null)
+        $record = $view.GetType().InvokeMember('Fetch', 'InvokeMethod', $null, $view, $null)
+
+        try {
+            $value = $record.GetType().InvokeMember('StringData', 'GetProperty', $null, $record, 1)
+        }
+        catch {
+            Write-Warning "Unable to get '$property' $($_.Exception.Message)"
+            continue
+        }
+        
+        if ($property -eq 'ProductLanguage') {
+            $value = "$value ($($productLanguageHashTable[$value]))"
         }
 
-        # Read property from MSI database
-        $WindowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-        $MSIDatabase = $WindowsInstaller.GetType().InvokeMember('OpenDatabase', 'InvokeMethod', $null, $WindowsInstaller, @($Path.FullName, 0))
+        $object | Add-Member -MemberType NoteProperty -Name $property -Value $value
+    }
 
-        foreach ($property in $properties) {
-            $View = $null
-            $Query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
-            $View = $MSIDatabase.GetType().InvokeMember('OpenView', 'InvokeMethod', $null, $MSIDatabase, ($Query))
-            $View.GetType().InvokeMember('Execute', 'InvokeMethod', $null, $View, $null)
-            $Record = $View.GetType().InvokeMember('Fetch', 'InvokeMethod', $null, $View, $null)
-            try {
-                $Value = $Record.GetType().InvokeMember('StringData', 'GetProperty', $null, $Record, 1) 
-            }
-            catch {
-                Write-Warning "Unable to get '$property' $($_.Exception.Message)"
-                continue
-            }
-            
-            $object | Add-Member -MemberType NoteProperty -Name $property -Value $Value
-        } 
 
-        # Commit database and close view
-        $MSIDatabase.GetType().InvokeMember('Commit', 'InvokeMethod', $null, $MSIDatabase, $null)
-        $View.GetType().InvokeMember('Close', 'InvokeMethod', $null, $View, $null)
+    $MSIDatabase.GetType().InvokeMember('Commit', 'InvokeMethod', $null, $MSIDatabase, $null)
+    $view.GetType().InvokeMember('Close', 'InvokeMethod', $null, $view, $null)
  
-        
-    }
-    End {
-        # Run garbage collection and release ComObject
-        $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WindowsInstaller) 
-        [System.GC]::Collect()
+    # Run garbage collection and release ComObject
+    $null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($windowsInstallerObject) 
+    [System.GC]::Collect()
 
-        return $object
-    }
+    return $object  
 } 
