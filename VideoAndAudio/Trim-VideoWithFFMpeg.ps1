184e4f88b38286eb28efb3be39bbe06e21bb982b
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-03-06 14:01:38 +0100

    Update Trim-VideoWithFFMpeg.ps1

--- VideoAndAudio/Trim-VideoWithFFMpeg.ps1
+++ VideoAndAudio/Trim-VideoWithFFMpeg.ps1
@@ -1,21 +1,49 @@
+Param(
+    [Parameter(Mandatory = $false)]
+    [string]$InputVideo,
+    [Parameter(Mandatory = $false)]
+    [string]$OutputVideo,
+    [Parameter(Mandatory = $true)]
+    [string]$StartCut,
+    [Parameter(Mandatory = $false)]
+    [string]$EndCut
+)
 
-# Define the input video file
-$inputVideo = "xxx"   
-$outputVideo = "xxx"
-$startCut = 45  # Starting cut time in seconds
-$endCut = 12    # Ending cut time in seconds
+# function to get time in hh:mm:ss format
+function Get-TimeInHHMMSSFormat {
+    param(
+        [string]$Time
+    )
+
+    $timeParts = $time.Split(':')
+
+    switch ($timeParts.Count) {
+        1 { return [TimeSpan]::FromSeconds([int]$timeParts[0]) }
+        2 { return [TimeSpan]::FromMinutes([int]$timeParts[0]).Add([TimeSpan]::FromSeconds([int]$timeParts[1])) }
+        3 { return [TimeSpan]::FromHours([int]$timeParts[0]).Add([TimeSpan]::FromMinutes([int]$timeParts[1])).Add([TimeSpan]::FromSeconds([int]$timeParts[2])) }
+        default { throw "Invalid time format. Please use one of the following formats: SS, MM:SS, or HH:MM:SS." }
+    }
+}
 
 # Get video duration using FFmpeg
-$duration = & ffmpeg -i $inputVideo 2>&1 | Select-String -Pattern "Duration: (\d+):(\d+):(\d+\.\d+)" | ForEach-Object { $_.Matches.Groups[1].Value, $_.Matches.Groups[2].Value, $_.Matches.Groups[3].Value -join ":" }
+$duration = & ffmpeg -i $InputVideo 2>&1 | Select-String -Pattern "Duration: (\d+):(\d+):(\d+\.\d+)" | ForEach-Object { $_.Matches.Groups[1].Value, $_.Matches.Groups[2].Value, $_.Matches.Groups[3].Value -join ":" }
 
-# Convert duration string to TimeSpan
-$durationTimeSpan = [TimeSpan]::Parse($duration)
+# Use the custom Parse-Time function
+$startCutTimeSpan = Get-TimeInHHMMSSFormat -Time $StartCut
 
-# Calculate new duration by subtracting 10 seconds (5 seconds from the start and 5 from the end)
-$newDuration = $durationTimeSpan.Subtract([TimeSpan]::FromSeconds($startCut + $endCut))
+# If no end cut time is provided, set it to the duration of the video
+if ([string]::IsNullOrWhitespace($EndCut)) {
+    Write-Host -ForegroundColor Cyan "No end cut time provided. Using the full duration of the video $duration"
+    $endCut = $duration
+}
+
+$endCutTimeSpan = Get-TimeInHHMMSSFormat -Time $EndCut
+
+# Calculate new duration by subtracting start cut from end cut
+$newDuration = $endCutTimeSpan.Subtract($startCutTimeSpan)
 
 # Format new duration for FFmpeg
 $newDuration = "{0:00}:{1:00}:{2:00}.{3:000}" -f $newDuration.Hours, $newDuration.Minutes, $newDuration.Seconds, $newDuration.Milliseconds
 
 # Use FFmpeg to cut the video
-& ffmpeg -ss 00:00:$startCut -i $inputVideo -t $newDuration -c copy $outputVideo
+& ffmpeg -ss $StartCut -i $InputVideo -t $newDuration -c copy $OutputVideo
